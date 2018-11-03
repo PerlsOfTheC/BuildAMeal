6ec6636c8d1173c193d6685aa1d8bc69c89007eb
Amanda Kwok <amandawkwok@gmail.com>
2018-11-02 17:01:09 -0700

    Moved addToCheckBox function to the js file & fixed scroll pane heights

--- src/homepageStyle.js
+++ src/homepageStyle.js
@@ -14,3 +14,18 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 });
+
+function addToCheckBox() {
+  // var val = $('#myInput').val();
+  // $('#ingredientCheckbox').append(val + "\n");
+  // $('#myInput').val('').focus();
+  var ul = document.getElementById("ingredientCheckbox");
+  var li = document.createElement("li");
+  var children = ul.children.length + 1;
+
+  li.setAttribute("id", "element"+children);
+  var val = $('#myInput').val();
+  li.appendChild(document.createTextNode(val));
+  ul.appendChild(li);
+   $('#myInput').val('').focus();
+}

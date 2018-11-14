4045b85fda9610c244c7431bec6f9a4728c84588
Walt <wpniederer@cpp.edu>
2018-11-13 21:35:10 -0800

    added skeleton functions for calling API

--- yummly_api/yummly.js
+++ yummly_api/yummly.js
@@ -1,5 +1,27 @@
 var request = require('request');
 
-var options = {
+if (yummlyKeys.appKeys && yummlyKeys.appID) {
+  var appQuery = '?_app_id=' + yummlyKeys.appID + '&_app_key=' + yummlyKeys.appKeys;
+}
+
+var Config = {
+  "endpoints": {
+    "recipe" : "https://api.yummly.com/v1/api/recipe/",
+    "recipes" : "https://api.yummly.com/v1/api/recipes",
+    "meta" : "https://api.yummly.com/v1/api/metadata/"
+  }
+};
+
+var validRequests = ['allergy,', 'course', 'cuisine', 'diet', 'ingredient'];
+
+function query(input) {
+
+}
+
+function getMetadata(url) {
+
+}
+
+function getDetails(recipeID) {
 
 }

0541b3b08fe8953bf862ee2a3ebd778c5fc93280
Walt <wpniederer@cpp.edu>
2018-11-15 16:23:24 -0800

    url builder for searching complete

--- yummly_api/yummly.js
+++ yummly_api/yummly.js
@@ -1,23 +1,57 @@
+require('dotenv').config();
+
 var request = require('request');
 
-if (yummlyKeys.appKeys && yummlyKeys.appID) {
-  var appQuery = '?_app_id=' + yummlyKeys.appID + '&_app_key=' + yummlyKeys.appKeys;
+yummlyKey = process.env.YUMMLY_API_KEY;
+yummlyID = process.env.YUMMLY_API_ID;
+console.log("MY_VARIABLE: " + yummlyKey);
+console.log("OTHER_VARIABLE: " + yummlyID);
+
+// If time permits throw an error
+if (typeof yummlyKey !== 'undefined' && typeof yummlyID !== 'undefined') {
+  var appQuery = '?_app_id=' + yummlyID + '&_app_key=' + yummlyKey;
+} else {
+  var appQuery = "No API keys passed to yummly";
 }
 
 var Config = {
   "endpoints": {
-    "recipe" : "https://api.yummly.com/v1/api/recipe/",
-    "recipes" : "https://api.yummly.com/v1/api/recipes",
-    "meta" : "https://api.yummly.com/v1/api/metadata/"
+    "recipeUrl" : "https://api.yummly.com/v1/api/recipe/",
+    "recipesUrl" : "https://api.yummly.com/v1/api/recipes",
+    "metaUrl" : "https://api.yummly.com/v1/api/metadata/"
   }
 };
 
 var validRequests = ['allergy,', 'course', 'cuisine', 'diet', 'ingredient'];
 
-function query(input) {
+function searchForRecipe(ingredientList) {
+  var url = Config.endpoints.recipesUrl + appQuery;
+
+  if (ingredientList && ingredientList.length > 0 && Array.isArray(ingredientList)) {
+    var searchQuery = '';
+    for (let i = 0; i < ingredientList.length; i++) {
+      if (typeof ingredientList[i] === 'string') {
+        searchQuery += ingredientList[i];
+        if (i >= 0 && i < ingredientList.length - 1) {
+          searchQuery += '+';
+        }
+      }
+    }
+    url += '&q=' + encodeURIComponent(searchQuery);
+    console.log('URL: ' + url);
+
+    return {
+      maxRecipeTime: function(input) {
+
+      }
+    }
+
+  }
 
 }
 
+console.log(searchForRecipe(['milk', 'eggs', 'chicken']));
+console.log(searchForRecipe(['onion', 'carrot', 'milk', 'brown sugar']));
 function getMetadata(url) {
 
 }

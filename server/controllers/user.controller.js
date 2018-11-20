ab1683edf7362d5969ee1ba0cec0d9614e5d645f
Leslie Covarrubias <lesliecovarrubias95@gmail.com>
2018-11-20 13:30:43 -0800

    saved reciepes function added saved reciepes function added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,23 +1,38 @@
-const bcrypt = require('bcrypt');
-const Joi = require('joi');
-const User = require('../models/user.model');
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const User = require("../models/user.model");
 
 const userSchema = Joi.object({
   fullname: Joi.string().required(),
   email: Joi.string().email(),
   mobileNumber: Joi.string().regex(/^[1-9][0-9]{9}$/),
   password: Joi.string().required(),
-  repeatPassword: Joi.string().required().valid(Joi.ref('password'))
-})
-
+  repeatPassword: Joi.string()
+    .required()
+    .valid(Joi.ref("password")),
+  myRecipes:[Joi.string()]
+});
 
 module.exports = {
-  insert
-}
+  insert,
+  saveRecipes,
+  deleteRecipes
+};
 
 async function insert(user) {
   user = await Joi.validate(user, userSchema, { abortEarly: false });
   user.hashedPassword = bcrypt.hashSync(user.password, 10);
   delete user.password;
   return await new User(user).save();
+}
+function saveRecipes(user, url) {
+  let usr = new User(user);
+  usr.myRecipes.push(url)
+  usr.save((item => { console.log('Recipe saved!'); });
+}
+
+function deleteRecipes(user, url) {
+  let usr = new User(user);
+  usr.myRecipes.pop(url)
+  usr.save((item => { console.log('Recipe removed!'); });
 }

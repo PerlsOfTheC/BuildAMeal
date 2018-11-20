72cd2cbae7f211e95aefb894961369cdc1484ef6
petre24 <pm_quin_24@yahoo.com>
2018-11-20 02:35:44 -0800

    Created the constructor for the recipe object in the search query

--- search_query_db.js
+++ search_query_db.js
@@ -1,8 +1,38 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
 	if (err) throw err;
-	console.log("Database is created!");
-	db.close();
+	function Recipe(sourceDisplayName, ingredients, id, smallImageUrls,
+                  totalCookTime, course, cuisine) {
+	  this.sourceDisplayName = sourceDisplayName;
+	  this.ingredientsList = ingredients;
+	  this.searchQuery = id;  // the search query for finding ingredients is the id
+	  this.smallImageUrls = smallImageUrls;
+	  this.totalCookTime = totalCookTime;
+	  this.course = course;
+	  this.cuisine = cuisine;
+  }
+
+	var dbo = db.db("mydb");
+	var recipeList = [
+    /*{ _id: 1, name: ["Bacon", "Mac and Cheese"], cooktime: "30 minutes"},
+    { _id: 2, name: "Sinigang", cooktime: "75 minutes"},
+    { _id: 3, name: "Scrambled Eggs", cooktime: "15 minutes"}, */
+    new Recipe()
+  ];
+	dbo.collection("recipes").insertMany(recipeList, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount)
+  });
+	dbo.collection("recipes").find().toArray(function(err, res) {
+	  if (err) throw err;
+	  console.log(res);
+	});
+	dbo.dropCollection("recipes", function(err, delOK) {
+	  if (err) throw err;
+	  if (delOK) console.log("Recipe collection deleted");
+	  db.close();
+  });
 });
+
